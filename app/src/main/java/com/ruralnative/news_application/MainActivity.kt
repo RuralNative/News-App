8db1e2e4ead1344d690c430952d527f669ae2f26
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-03 14:01:16 +0800

    Removed Default UI Code
    Removed the default UI codebase from the Activity class as it will not be needed at all.

--- app/src/main/java/com/ruralnative/news_application/MainActivity.kt
+++ app/src/main/java/com/ruralnative/news_application/MainActivity.kt
@@ -17,30 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NewsApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TODO("Add UI Content")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsApplicationTheme {
-        Greeting("Android")
     }
 }
