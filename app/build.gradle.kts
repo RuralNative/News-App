357aec4ae5af781b7558ffd01ac64996668ca1e0
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-06 18:51:49 +0800

    Updated Gradle Configuration
    Injected the utilities module inside the app module, and updated the Kotlin gradle plugin from 1.9.20 to 1.9.22 to adapt with the Kotlin Sympol Processor plugin used for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(project(":utilities"))
 
     implementation(enforcedPlatform(libs.compose.bom))
     implementation(libs.compose.ui)
